a88e2cf752470915860c1658d6e93b3a2edf10c7
Ignacio Quiroga <ignacioequiroga@gmail.com>
2020-06-07 21:27:03 -0300

    modificando middleware para validar mail de usuario

--- site/middleware/userRegisterValidator.js
+++ site/middleware/userRegisterValidator.js
@@ -1,23 +1,52 @@
-const {check, valiationResult, body} = require('express-validator');
+const { check, valiationResult, body } = require('express-validator');
 const usersController = require('../controllers/usersController')
 
-const emailMiddleware = {
-    verifyEmail: async (req,res,next) =>{
-       try{
-        [body('email').custom(function(value){
-            const users = usersController.users();
-            console.log(users);
-            const userFound =  users.find(user => {
-                return user.email == value;
-            })
-            if(userFound){
-                return true
-            } return false
-        }).withMessage('E-mail ya existente')]
-       }catch(error){
-        return next(error)
-       }
-    }
+const reglasDeValidacionDeUsuarios = () => {
+    return [body('email').custom(function (value) {
+        const users = usersController.users();
+        const userFound = users.find(user => {
+            return user.email == value;
+        })
+        if (userFound) {
+            return false;
+        } return true;
+    }).withMessage('E-mail ya existente'), check("password", "Password Invalido, minimo 4 caracteres")
+        .isLength({ min: 4 })
+        .custom((value, { req, loc, path }) => {
+            if (value !== req.body.repeatPassword) {
+                // trow error if passwords do not match
+                throw new Error("Los passwords no coinciden");
+            } else {
+                return value;
+            }
+        })
+    ]
 }
 
-module.exports = emailMiddleware
+const validar = (req, res, next) => {
+    next();
+};
+
+module.exports = {reglasDeValidacionDeUsuarios, validar}
+
+
+/*const emailMiddleware = {
+    verifyEmail: async (req, res, next) => {
+        try {
+            [body('email').custom(function (value) {
+                const users = usersController.users();
+                console.log(users);
+                const userFound = users.find(user => {
+                    return user.email == value;
+                })
+                if (userFound) {
+                    return true
+                } return false
+            }).withMessage('E-mail ya existente')]
+        } catch (error) {
+            return next(error)
+        }
+    }
+}*/
+
+
--- site/routes/users.js
+++ site/routes/users.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 const loginController = require('../controllers/loginController');
-const usersController = require('../controllers/usersController')
-const {check, valiationResult, body} = require('express-validator');
 const multer = require('multer');
+const {reglasDeValidacionDeUsuarios, validar} = require('../middleware/userRegisterValidator');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -19,24 +18,6 @@ var upload = multer({ storage: storage })
 router.get('/login', loginController.login )
 router.post('/login', loginController.verify )
 router.get('/login/register', loginController.register )
-router.post('/login/register',upload.single('avatar'),[body('email').custom(function(value){
-        const users = usersController.users();
-        const userFound =  users.find(user => {
-            return user.email == value;
-        })
-        if(userFound){
-            return false;
-        } return true;
-    }).withMessage('E-mail ya existente'),check("password", "Password Invalido, minimo 4 caracteres")
-    .isLength({ min: 4 })
-    .custom((value,{req, loc, path}) => {
-        if (value !== req.body.repeatPassword) {
-            // trow error if passwords do not match
-            throw new Error("Los passwords no coinciden");
-        } else {
-            return value;
-        }
-    })
-], loginController.addUser)
+router.post('/login/register',upload.single('avatar'),reglasDeValidacionDeUsuarios(), validar, loginController.addUser)
 
 module.exports = router;
