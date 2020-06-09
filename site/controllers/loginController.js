bbe74bd838b332b49a8f4a1b96462ef2c9898877
Francisco Olmos <francisco.olmos.ubp@gmail.com>
2020-06-09 00:48:05 -0300

    Se modifica el registro del usuario, ahora por defecto todos los usuarios registrados tienen categoria 2 = user

--- site/controllers/loginController.js
+++ site/controllers/loginController.js
@@ -38,7 +38,8 @@ const loginController = {
         lastName: req.body.lastName,
         email: req.body.email,
         password: bcrypt.hashSync(req.body.password, 10),
-        avatar: req.file.originalname
+        avatar: req.file.originalname,
+        category: 2
       };
       jsonOperations.addToJSON(userToAdd, path.join('site', 'data', 'users.json'));
       res.redirect('/products');
