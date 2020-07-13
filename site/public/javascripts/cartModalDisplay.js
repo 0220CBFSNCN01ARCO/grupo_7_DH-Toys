793698bff4946b03120094d90987c0fd92271c7f
Francisco Olmos <francisco.olmos@unxdigital.com>
2020-07-13 16:10:07 -0300

    Se agrega nueva animación al sidebar del carrito

--- site/public/javascripts/cartModalDisplay.js
+++ site/public/javascripts/cartModalDisplay.js
@@ -1,24 +1,44 @@
-window.onload = () => {
+window.addEventListener('load', () => {
 
-    document.querySelector('#sidebar_toggle_btn').addEventListener('click', function(e){
-        const cartModal = document.querySelectorAll(".hide")
-        cartModal.forEach(element => {
-          if (element.style.display === "none") {
-            element.style.display = "block";
-          } else {
-            element.style.display = "none";
-          }
-        })
-    })
-    
-    document.querySelector(".close-icon-black").addEventListener('click', function(e){
-      const cartModal = document.querySelectorAll(".hide")
-      cartModal.forEach(element => {
-        if (element.style.display === "none") {
-          element.style.display = "block";
-        } else {
-          element.style.display = "none";
-        }
-      })
-    })
-}
+  let menuSidebar = document.querySelector('.sidebar-right');
+  let menuSidebarNav = document.querySelector('#sidebar_toggle_btn');
+  let menuSidebarClose = document.querySelector('#sidebar_close_icon');
+  let menuSidebarOverlay = document.querySelector('.sidebar_overlay');
+  let overlay = document.querySelector('.sidebar_overlay');
+
+  menuSidebarNav.addEventListener('click', function(e){
+    e.preventDefault();
+    menuSidebarNav.classList.toggle('active');
+    menuSidebar.classList.toggle('sidebar-open');
+    menuSidebarOverlay.classList.toggle('sidebar_overlay_active');
+  })
+
+  menuSidebarClose.addEventListener('click', function(e){
+    menuSidebarNav.classList.remove('active');
+    menuSidebar.classList.remove('sidebar-open');
+    menuSidebarOverlay.classList.remove('sidebar_overlay_active');
+  })
+
+  overlay.addEventListener('click', function(e){
+    menuSidebarNav.classList.toggle('active');
+    menuSidebar.classList.toggle('sidebar-open');
+    menuSidebarOverlay.classList.toggle('sidebar_overlay_active');
+  })
+  /*
+  document.querySelector('#sidebar_toggle_btn').addEventListener('click', function (e) {
+    e.preventDefault()
+    document.querySelector('#sidebar-right').classList.toggle('sidebar-open')
+    document.querySelector("#sidebar-right").classList.toggle('hide')
+    document.querySelector(".sidebar_overlay").classList.toggle('hide')
+  })
+
+  document.querySelector(".close-icon-black").addEventListener('click', function (e) {
+    document.querySelector("#sidebar-right").classList.toggle('hide')
+    document.querySelector(".sidebar_overlay").classList.toggle('hide')
+  })
+
+  document.querySelector(".sidebar_overlay").addEventListener('click', function (e) {
+    document.querySelector("#sidebar-right").classList.toggle('hide')
+    document.querySelector(".sidebar_overlay").classList.toggle('hide')
+  })*/
+})
